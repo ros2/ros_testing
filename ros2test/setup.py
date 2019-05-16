a0a9420d5ef008cfd121395c954306d70be34542
Michel Hidalgo <michel@ekumenlabs.com>
2019-05-16 13:14:54 -0300

    Change ros2test and ros_testing versions.
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>

--- ros2test/setup.py
+++ ros2test/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ros2test',
-    version='0.8.1',
+    version='0.1.0',
     packages=find_packages(exclude=['test']),
     install_requires=['ros2cli'],
     zip_safe=True,
