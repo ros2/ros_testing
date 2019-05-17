c69c48eb07253d6ce4dab431ce37e026dfca184f
Michel Hidalgo <michel@ekumenlabs.com>
2019-05-17 01:10:39 -0300

    Add ros_testing metapackage and ros2test CLI package (#1)
    * Add ros2 test CLI command.
    
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>
    
    * Add ros2 testing metapackage.
    
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>
    
    * Add add_ros_test() CMake convenience macro.
    
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>
    
    * Apply minor fixes to ros2 test CLI command.
    
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>
    
    * Drop ROS2 specific naming.
    
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>
    
    * Apply misc fixes to the ros_testing package.
    
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>
    
    * Apply misc fixes to the ros2test package.
    
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>
    
    * Change ros2test and ros_testing versions.
    
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>

---
+++ ros2test/ros2test/command/test.py
@@ -0,0 +1,32 @@
+# Copyright 2019 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import launch_testing.launch_test
+import launch_testing_ros
+
+from ros2cli.command import CommandExtension
+
+
+class TestCommand(CommandExtension):
+    """Run a ROS2 launch test."""
+
+    def add_arguments(self, parser, cli_name):
+        """Add arguments to argparse."""
+        launch_testing.launch_test.add_arguments(parser)
+
+    def main(self, *, parser, args):
+        """Entry point for CLI program."""
+        return launch_testing.launch_test.run(
+            parser, args, test_runner_cls=launch_testing_ros.LaunchTestRunner
+        )
---
+++ ros2test/setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='ros2test',
+    version='0.1.0',
+    packages=find_packages(exclude=['test']),
+    install_requires=['ros2cli'],
+    zip_safe=True,
+    author='Michel Hidalgo',
+    author_email='michel@ekumenlabs.com',
+    maintainer='Michel Hidalgo',
+    maintainer_email='michel@ekumenlabs.com',
+    url='https://github.com/ros2/rostest/tree/master/ros2test',
+    download_url='https://github.com/ros2/rostest/releases',
+    keywords=[],
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+    ],
+    description='The test command for ROS 2 launch tests.',
+    long_description="""\
+This package provides the test command for ROS 2 launch tests.""",
+    license='Apache License, Version 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'ros2cli.command': [
+            'test = ros2test.command.test:TestCommand',
+        ],
+    }
+)
